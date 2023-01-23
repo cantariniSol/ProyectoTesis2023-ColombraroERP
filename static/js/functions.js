f291e1e6b0dcf70e099ccefc4eb60f5cc2050d36
solCantarini <cantarinisol@hotmail.com>
2023-01-22 22:29:53 -0300

    Alert con jQuery Alert

--- static/js/functions.js
+++ static/js/functions.js
@@ -16,3 +16,51 @@ function message_error(obj) {
     });
 };
 
+function submit_with_ajax(url, title, content, parameters, callback) {
+    $.confirm({
+        theme: 'material',
+        title: title,
+        icon: 'fa fa-info',
+        content: content,
+        columnClass: 'small',
+        typeAnimated: true,
+        cancelButtonClass: 'btn-warning',
+        draggable: true,
+        dragWindowBorder: false,
+        buttons: {
+            info: {
+                text: "Si",
+                btnClass: 'btn-success',
+                action: function () {
+                    $.ajax({
+                        url: url, //window.location.pathname
+                        type: 'POST',
+                        data: parameters,
+                        dataType: 'json',
+                        processData: false,
+                        contentType: false,
+                    }).done(function (data) {
+                        console.log(data);
+                        if (!data.hasOwnProperty('error')) {
+                            callback();
+                            return false;
+                        }
+                        message_error(data.error);
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                        alert(textStatus + ': ' + errorThrown);
+                    }).always(function (data) {
+
+                    });
+                }
+            },
+            danger: {
+                text: "No",
+                btnClass: 'btn-danger',
+                action: function () {
+
+                }
+            },
+        }
+    })
+}
+
